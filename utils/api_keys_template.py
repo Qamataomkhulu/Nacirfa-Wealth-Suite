a1e36082ead20de16b4792d422b9e9fdb292f9f3
Qamataomkhulu <iambandile@icloud.com>
2025-11-05 20:24:54 +0200

    Update api_keys_template.py

--- utils/api_keys_template.py
+++ utils/api_keys_template.py
@@ -1,1 +1,15 @@
+"""
+api_keys_template.py
+--------------------
+Rename this to api_keys.py and fill in your keys.
+This file is ignored by .gitignore.
+"""
+
+BINANCE_API_KEY = "your_binance_api_key_here"
+BINANCE_SECRET_KEY = "your_binance_secret_here"
+
+FRED_API_KEY = "your_fred_api_key_here"
+
+# Example usage:
+# from utils.api_keys import BINANCE_API_KEY
 
